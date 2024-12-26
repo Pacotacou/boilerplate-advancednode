ddef2d584643256eb5e1dcedcceaf768de852edb
Pacotacou <p4c0taco@gmail.com>
2024-12-25 23:43:22 -0500

    feat: set github strategy

--- auth.js
+++ auth.js
@@ -3,5 +3,14 @@ require('dotenv').config();
 const passport = require('passport');
 
 module.exports = function (app, myDataBase){
-
+    passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: 'https://sussex-quotations-rank-ones.trycloudflare.com/'
+      },
+        function(accessToken, refreshToken, profile, cb) {
+          console.log(profile);
+          //Database logic here with callback containing your user object
+        }
+      ));
 }
