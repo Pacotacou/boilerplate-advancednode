fae4912ce7a24d0dca9270968466a5619b5164c7
Pacotacou <p4c0taco@gmail.com>
2024-12-25 22:47:53 -0500

    feat: routes is clean

--- routes.js
+++ routes.js
@@ -1,3 +1,73 @@
-module.expors = function (app, myDataBase){
+
+const passport = require('passport');
+
+module.exports = function (app, myDataBase){
+
+    function ensureAuthenticated(req,res,next){
+        if (req.isAuthenticated()){
+        return next();
+        }
+        res.redirect('/');
+    };
+
+    app.route('/')
+    .get(function(req, res){
+      res.render('index',{
+        title:"Connected to Database",
+        message:"Please log in",
+        showLogin: true,
+        showRegistration: true
+      });
+    });
+  
+    app.route('/login')
+        .post(
+        passport.authenticate('local',{failureRedirect: '/'}),
+        function(req,res){
+            res.redirect('/profile');
+        }
+        );
+
     
+
+    app.route('/logout')
+        .get(function(req,res){
+        req.logout();
+        res.redirect('/');
+        })
+  
+    app.route('/profile')
+        .get(ensureAuthenticated, function(req,res){
+        res.render('profile',{
+            username: req.user.username,
+        });
+        });
+  
+    app.route('/register')
+        .post(function(req,res,next){
+        myDataBase.findOne({ username: req.body.username}, function(err,user){
+            if (err){
+            next(err);
+            }else if (user){
+            res.redirect('/');
+            }else {
+            const hash = bcrypt.hashSync(req.body.password,12);
+            myDataBase.insertOne({
+                username: req.body.username,
+                password: hash
+            },function(err,doc){
+                if(err){
+                res.redirect('/');
+                }else{
+                // the inserted document is held within
+                // the ops property of the doc
+                next(null,doc.ops[0])
+                }
+            })
+            }
+        }),
+        passport.authenticate('local', {failureRedirect: '/'}),
+        function(req,res,next){
+        res.redirect('/profile');
+        }});
 }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-require('./routes.js');
+const routes = require('./routes.js');
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
@@ -30,73 +30,8 @@ app.use(passport.session())
 
 myDB(async function(client){
   const myDataBase = await client.db('database').collection('users');
-  
-  app.route('/')
-    .get(function(req, res){
-      res.render('index',{
-        title:"Connected to Database",
-        message:"Please log in",
-        showLogin: true,
-        showRegistration: true
-      });
-    });
-  
-  app.route('/login')
-    .post(
-      passport.authenticate('local',{failureRedirect: '/'}),
-      function(req,res){
-        res.redirect('/profile');
-      }
-    );
 
-  function ensureAuthenticated(req,res,next){
-    if (req.isAuthenticated()){
-      return next();
-    }
-    res.redirect('/');
-  };
-
-  app.route('/logout')
-    .get(function(req,res){
-      req.logout();
-      res.redirect('/');
-    })
-  
-  app.route('/profile')
-    .get(ensureAuthenticated, function(req,res){
-      res.render('profile',{
-        username: req.user.username,
-      });
-    });
-  
-  app.route('/register')
-    .post(function(req,res,next){
-      myDataBase.findOne({ username: req.body.username}, function(err,user){
-        if (err){
-          next(err);
-        }else if (user){
-          res.redirect('/');
-        }else {
-          const hash = bcrypt.hashSync(req.body.password,12);
-          myDataBase.insertOne({
-            username: req.body.username,
-            password: hash
-          },function(err,doc){
-            if(err){
-              res.redirect('/');
-            }else{
-              // the inserted document is held within
-              // the ops property of the doc
-              next(null,doc.ops[0])
-            }
-          })
-        }
-      }),
-    passport.authenticate('local', {failureRedirect: '/'}),
-    function(req,res,next){
-      res.redirect('/profile');
-    }
-    });
+  routes(app, myDataBase);
 
   app.use(function(req,res,next){
     res.status(404)
