38482dd55d3c239fb244090ba0c991d78e03c4bb
Pacotacou <p4c0taco@gmail.com>
2024-12-26 11:52:27 -0500

    feat: add chat with socket.io

--- server.js
+++ server.js
@@ -1,103 +1,100 @@
 'use strict';
-const routes = require('./routes.js');
-const auth = require('./auth.js');
-
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
-const {ObjectID} = require('mongodb');
-const LocalStrategy = require('passport-local');
-const bcrypt = require('bcrypt');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI})
 
 const app = express();
+
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.set('view engine','pug');
-app.set('views','./views/pug');
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: {secure:false}
+  cookie: { secure: false }
 }));
-app.use(passport.initialize())
-app.use(passport.session())
 
-myDB(async function(client){
+app.use(passport.initialize());
+app.use(passport.session());
+
+fccTesting(app); // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
-  let currentUsers = 0;
-  io.on('connection',function(socket){
-    console.log('A user has connected');
-    ++currentUsers;
-    io.emit('user count',currentUsers);
+  routes(app, myDataBase);
+  auth(app, myDataBase);
 
-    socket.on('disconnect',function(){
+  function onAuthorizeSuccess(data,accept){
+    console.log('successful connection to socket.io');
+    accept(null,true);
+  }
+
+  function onAutorizeFail(data,message,error,accept){
+    if (error) throw new Error(message);
+    console.log('failed connection to socket.io',message);
+    accept(null,false);
+  }
+
+  io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: 'express.sid',
+      secret: process.env.SESSION_SECRET,
+      store: store,
+      success: onAuthorizeSuccess,
+      fail: onAutorizeFail
+    })
+  )
+  let currentUsers = 0;
+  io.on('connection', (socket) => {
+    ++currentUsers;
+    io.emit('user', {
+      username: socket.request.user.username,
+      currentUsers,
+      connected:true
+    });
+    console.log('A user has connected');
+    console.log('user '+ socket.request.user.username + ' connected')
+
+    socket.on('chat message', function(message){
+      io.emit('chat message', {username: socket.request.user.username,
+        message
+      });
+    });
+
+    socket.on('disconnect', () => {
       console.log('A user has disconnected');
       --currentUsers;
       io.emit('user count', currentUsers);
     });
   });
-
-  routes(app, myDataBase);
-
-  app.use(function(req,res,next){
-    res.status(404)
-      .type('text')
-      .send('Not Found');
-  })
-
-  passport.serializeUser(function(user,done){
-    done(null,user._id);
-  });
-  passport.deserializeUser(function(id,done){
-    myDataBase.findOne({_id: new ObjectID(id)}, function(err,doc){
-      done(null,doc);
-    });
-  });
-
-  passport.use(new LocalStrategy(function(username, password, done){
-    myDataBase.findOne({username:username},function(err,user){
-      console.log(`User ${username} attempted to log in.`);
-      if (err) return done(err);
-      if (!user) return done(err);
-      if (!bcrypt.compareSync(password, user.password)) {
-        return done(null, false);
-      }
-      /*if (password !== user.password) return done(null,false);
-      return done(null,user);*/
-    });
-  }));
-
-  auth(app,myDataBase);
-
   
-
-
-}).catch(function(e){
-  app.route('/').get(function(req,res){
-    res.render('index',{
-      title: e,
-      message: 'Unable to connect to database'
-    });
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
-
+  
 const PORT = process.env.PORT || 3000;
-/*app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
-});*/
 http.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log(`Listening on port ${PORT}`);
 });
-
